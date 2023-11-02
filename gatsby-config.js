2ce08827ef2042a6731137011e4130a77df1fc01
Paoran <118447416+PaoranCSG@users.noreply.github.com>
2023-11-02 10:35:31 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     // Site URL for when it goes live
-    siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
+    siteUrl: `https://paorancsg.github.io/`,
     // Your Name
     name: 'Niklas Malinen',
     // Main Site Title
